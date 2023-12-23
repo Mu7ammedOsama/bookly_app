909e9e661758191260fd4ec8a26a096a2df2ee36
Muhammed Osama <MuhammedOsama.Dev@gmail.com>
2023-12-23 17:17:35 +0200

    Add Slide Animation to Splash View

--- lib/features/splash/presentation/views/widgets/splashViewBody.dart
+++ lib/features/splash/presentation/views/widgets/splashViewBody.dart
@@ -3,8 +3,42 @@ import 'package:flutter/material.dart';
 // core
 import 'package:bookly_app/core/utilities/assetsData.dart';
 
-class SplashViewBody extends StatelessWidget {
+// features
+import 'package:bookly_app/features/splash/presentation/views/widgets/slideText.dart';
+
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 2),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +48,7 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Image.asset(AssetsData.logo),
         const SizedBox(height: 8),
-        const Text(
-          'Read Free Book',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 16),
-        ),
+        SlideText(slideAnimation: slideAnimation),
       ],
     );
   }
