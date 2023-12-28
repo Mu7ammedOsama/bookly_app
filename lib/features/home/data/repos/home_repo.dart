85f1961debafe225ec073c1cb799bb9c6ba45230
Muhammed Osama <MuhammedOsama.Dev@gmail.com>
2023-12-28 20:05:28 +0200

    Install Dartz, Create Home Repo & Failures

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,5 +1,9 @@
-abstract class HomeRepo {
-  fetchFeaturedBooks();
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
 
-  fetchBestSellerBooks();
+abstract class HomeRepo {
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks();
+
+  Future<List<BookModel>> fetchBestSellerBooks();
 }
