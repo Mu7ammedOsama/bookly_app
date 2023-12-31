d002d22a2ae2dd9b0f2aed437c7ae49504bcde6c
Muhammed Osama <MuhammedOsama.Dev@gmail.com>
2023-12-31 21:24:24 +0200

    Code Refactoring

--- lib/core/utilities/assets_data.dart
+++ lib/core/utilities/assets_data.dart
@@ -1,13 +1,3 @@
 class AssetsData {
   static const logo = 'assets/images/logo.png';
-  static const main1 = 'assets/images/main1.png';
-  static const main2 = 'assets/images/main2.png';
-  static const main3 = 'assets/images/main3.png';
-  static const b1 = 'assets/images/b1.png';
-  static const b2 = 'assets/images/b2.png';
-  static const b3 = 'assets/images/b3.png';
-  static const b4 = 'assets/images/b4.png';
-  static const b5 = 'assets/images/b5.png';
-  static const b6 = 'assets/images/b6.png';
-  static const b7 = 'assets/images/b7.png';
 }
